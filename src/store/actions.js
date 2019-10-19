faeecdc53185ce62d3b50ffb42037960191503ba
anjwoc <anjwoc@naver.com>
2019-10-19 23:18:10 +0900

    delete footer

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,22 @@
 // https://vuex.vuejs.org/en/actions.html
 
 export default {
-  //
+  signUp({commit, state}, payload){
+    this.$axios.post('/user', {
+      email: payload.email,
+      nickname: payload.nickname,
+      password: payload.password,
+    },{
+      withCredentials: true,
+    })
+      .then((res)=>{
+        console.log(res);
+        commit('setMe', res.data);
+      })
+      .catch((err)=>{
+        console.error(err);
+      });
+  },
+
+
 }
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,5 +1,7 @@
 // https://vuex.vuejs.org/en/mutations.html
 
 export default {
-  //
+  setMe(state, payload){
+    state.me = payload;
+  }
 }
